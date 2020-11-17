d996bb2eb9782708b74333511dd1b13fda8c1911
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2020-11-17 09:31:22 +0200

    Change projekt
    Изменил проект смены темы от Ресо Кодера, на стандартный проект счетчика. Потому, что тот проект слишком сильно устарел.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
+//import 'package:flutter_bloc/flutter_bloc.dart';
+import 'bloc/counter_observer.dart';
+import 'app.dart';
 
-import 'view/home_page.dart';
-import 'bloc/theme_bloc.dart';
-import 'theme/theme_state.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      builder: (context) => ThemeBloc(),
-      child: BlocBuilder<ThemeBloc, ThemeState>(
-        builder: _buildWithTheme,
-      ),
-    );
-  }
-
-  Widget _buildWithTheme(BuildContext context, ThemeState state) {
-    return MaterialApp(
-      title: 'MaterialApp',
-      home: HomePage(),
-      theme: state.themeData,
-    );
-  }
+void main() {
+  Bloc.observer = CounterObserver(); // Создается для реагирования на все переходы
+    // Блок делегат. По сути наблюдает за всеми изменениями состояний в приложении
+    // Создается наследованием от BlocObserver 
+    // и перегрузкой метода onChange()
+  runApp(const CounterApp());
 }
