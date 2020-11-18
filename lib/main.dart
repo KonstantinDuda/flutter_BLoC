9c5c914120dce2999c76423d7c1c271e84f19352
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2020-11-18 10:38:08 +0200

    ThemeSwitch
    Переписал программу. Теперь тут и смена тебя и счетчик. TODO: Раскидать по разным файлам

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,180 @@
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
-//import 'package:flutter_bloc/flutter_bloc.dart';
-import 'bloc/counter_observer.dart';
-import 'app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:async';
+/*
+import 'view/home_page.dart';
+import 'bloc/theme_bloc.dart';
+import 'theme/theme_state.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      builder: (context) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: _buildWithTheme,
+      ),
+    );
+  }
+
+  Widget _buildWithTheme(BuildContext context, ThemeState state) {
+    return MaterialApp(
+      title: 'MaterialApp',
+      home: HomePage(),
+      theme: state.themeData,
+    );
+  }
+}*/
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    print(event);
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onChange(Cubit cubit, Change change) {
+    print(change);
+    super.onChange(cubit, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(cubit, error, stackTrace);
+  }
+}
 
 void main() {
-  Bloc.observer = CounterObserver(); // Создается для реагирования на все переходы
-    // Блок делегат. По сути наблюдает за всеми изменениями состояний в приложении
-    // Создается наследованием от BlocObserver 
-    // и перегрузкой метода onChange()
-  runApp(const CounterApp());
+  Bloc.observer = SimpleBlocObserver();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, ThemeData>(
+        builder: (_, theme) {
+          return MaterialApp(
+            theme: theme,
+            home: BlocProvider(
+              create: (_) => CounterBloc(),
+              child: CounterPage(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter on SwitchTheme')),
+      body: BlocBuilder<CounterBloc, int>(
+        builder: (_, count) {
+          return Center(
+            child: Text(
+              '$count',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () =>
+                context.read<CounterBloc>().add(CounterEvent.increment),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: const Icon(Icons.remove),
+              onPressed: () => 
+                context.read<CounterBloc>().add(CounterEvent.decrement),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: const Icon(Icons.brightness_6),
+              onPressed: () => context.read<ThemeCubit>().toggleTheme(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              backgroundColor: Colors.red,
+              child: const Icon(Icons.error),
+              onPressed: () => context.read<CounterBloc>().add(null),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum CounterEvent {increment, decrement}
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0);
+
+  @override 
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+      default:
+        addError(Exception('unsupported event'));
+    }
+  }
+}
+
+class ThemeCubit extends Cubit<ThemeData> {
+  ThemeCubit() : super(_lightTheme);
+
+  static final _lightTheme = ThemeData(
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Colors.white,
+    ),
+    brightness: Brightness.light,
+  );
+
+  static final _darkTheme = ThemeData(
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      foregroundColor: Colors.black,
+    ),
+    brightness: Brightness.dark,
+  );
+
+  void toggleTheme() {
+    emit(state.brightness == Brightness.dark ? _lightTheme : _darkTheme);
+  }
 }
