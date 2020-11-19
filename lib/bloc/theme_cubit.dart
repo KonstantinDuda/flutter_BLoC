8586e1292369d418f3790a544f74b6bfef6e0d0e
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2020-11-19 19:34:38 +0200

    Change headline6
    Переопределил в темах цвета текстов и их шрифт. Перегрузил headline6.

--- lib/bloc/theme_cubit.dart
+++ lib/bloc/theme_cubit.dart
@@ -9,14 +9,30 @@ class ThemeCubit extends Cubit<ThemeData> {
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
       foregroundColor: Colors.white,
     ),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        color: Colors.white,
+        fontSize: 28.0,
+      ),
+    ),
     brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    accentColor: Colors.blue,
   );
 
   static final _darkTheme = ThemeData(
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
       foregroundColor: Colors.black,
     ),
+    textTheme: TextTheme(
+      headline6: TextStyle(
+        color: Colors.black,
+        fontSize: 28.0,
+      ),
+    ),
     brightness: Brightness.dark,
+    primaryColor: Colors.orange,
+    accentColor: Colors.orange,
   );
 
   void toggleTheme() {
