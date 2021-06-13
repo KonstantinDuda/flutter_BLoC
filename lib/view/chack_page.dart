897d6f9d9cbcf8b664e7a381617422c14c4cc2c5
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2021-06-13 17:35:15 +0300

    New title

--- lib/view/chack_page.dart
+++ lib/view/chack_page.dart
@@ -1,116 +1,143 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../bloc/chack_task_bloc.dart';
 import '../bloc/provider_bloc.dart';
-import '../bloc/counter_bloc.dart';
 //import '../bloc/theme_cubit.dart';
-import 'my_drawer.dart';
-//import 'my_dialog.dart';
+
+import '../database/chack_task.dart';
+import '../database/chack_task_event.dart';
+import '../database/chack_task_state.dart';
 
 class ChackPage extends StatelessWidget {
+  //String id;
+  final String title;
+  ChackPage(this.title);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Counter on SwitchTheme. ChackPage')),
-      drawer: MyDrawer(),  /*Drawer(
-        child: Center( 
-          child: Container(
-            width: 50.0,
-            height: 50.0,
-            child: FloatingActionButton(
-              mini: true,
-              child: Icon(Icons.brightness_6),
-              onPressed: () => context.read<ThemeCubit>().toggleTheme(),
-            ),
+    //var borderColor = Theme.of(context).accentColor;
+    print("build RootPage");
+    return BlocBuilder<ChackTaskBloc, ChackTaskState>(builder: (context, state) {
+      List<ChackTask> tasks;
+      if (state is TaskLoadSuccessState) {
+        if (state.tasks == null) {
+          tasks = [];
+        } else
+          tasks = state.tasks;
+      } else {
+        tasks = [];
+      }
+      print('tasks == $tasks');
+      return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back), 
+            onPressed: () {
+              BlocProvider.of<ProviderBloc>(context)
+                    .add(RootEvent());
+            },
           ),
+          title: Text(title),
         ),
-      ),*/
-      // Блок строитель обрабатывает создание виджета в 
-      // ответ на новое состояние. 
-      // (потенциально) Функция строитель может 
-      // вызываться несколько раз.
-      // Является дженериком. Использует класс управляющий 
-      // состоянием и состояние как второй елемент
-      body: BlocBuilder<CounterBloc, int>(
-        builder: (_, count) {
-          return Center(
-            child: Container(
-              //width: 50.0,
-              //height: 50.0,
-              child: FloatingActionButton(
-                child: Center(
-                child: Text(
-                // Выводим каунт которым управляет и который
-                // прослушивает Блок строитель
-                '$count',
-                // Указываем стиль текста, чтоб менялся цвет
-                // когда меняет тему приложения
-                style: Theme.of(context).textTheme.headline6,
-                ),),
-                onPressed: (){
-                  //Navigator.of(context).pop();
-                  BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                  //BlocProvider.of<ProviderBloc>(context).add(ProviderEvent.rootPage);
+        body: ListView.builder(
+            itemCount: tasks == []
+                ? 0
+                : tasks.length, 
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  print("onTap: ${tasks[index].text}");
                 },
-              ),
-              //),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          /*Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: const Icon(Icons.add),
-              onPressed: () =>
-                context.read<CounterBloc>().add(CounterEvent.increment),
-            ),
-          ),*/
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: const Icon(Icons.remove),
-              onPressed: () => 
-                // Благодаря тому, что у нас есть доступ к блоку счетчика
-                // так-как мы добавили его в провайдере
-                // теперь мы можем считать из контекста с указанием 
-                // класса управляющего счетчиком и добавить событие
-                context.read<CounterBloc>().add(CounterEvent.decrement),
-            ),
-          ),/*
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              child: const Icon(Icons.brightness_6),
-              // Благодаря тому, что у нас есть доступ к кубиту темы
-              // так-как мы добавили его в провайдере
-              // теперь мы можем считать из контекста с указанием 
-              // класса управляющего темой и вызвать событие
-              onPressed: () => context.read<ThemeCubit>().toggleTheme(),
-            ),
-          ),*/
-          /*Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: FloatingActionButton(
-              backgroundColor: Colors.red,
-              child: const Icon(Icons.error),
-              onPressed: () /*async {
-                await Navigator.push(context, 
-                  PageRouteBuilder(
-                    opaque: false, // Открывать не на полный экран
-                    pageBuilder: (context, _, __) => MyDialog()),
-                ); 
-              },*/
-              => context.read<CounterBloc>().add(null),
-            ),
-          ),*/
-        ],
-      ),
-    );
+                onLongPress: () {
+                  print('longPress on ${tasks[index].text}');
+                  /*BlocProvider.of<ProviderBloc>(context)
+                      .add(UpdateEvent(tasks[index]));*/
+                },
+                onHorizontalDragStart: (DragStartDetails start) {
+                  print(start);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      duration: Duration(seconds: 3),
+                      content: Row(
+                        children: <Widget>[
+                          CircularProgressIndicator(
+                            //value: 0.4,
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                              child: Text("Delete ${tasks[index].text}?"),
+                            ),
+                          ),
+                          TextButton(
+                            style: ButtonStyle(
+                              minimumSize: MaterialStateProperty.all<Size>(
+                                  Size(80.0, 50.0)),
+                              backgroundColor:
+                                  MaterialStateProperty.all<Color>(Theme.of(context).accentColor),
+                            ),
+                            child: Text("yes"),
+                            onPressed: () {
+                              BlocProvider.of<ChackTaskBloc>(context)
+                                  .add(ChackTaskDeletedEvent(tasks[index].id));
+                              ScaffoldMessenger.of(context)
+                                  .removeCurrentSnackBar();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).accentColor,
+                      width: 1.5,
+                    ),
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Theme.of(context).textTheme.headline6.color,
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          //color: Colors.red,
+                          margin: EdgeInsets.fromLTRB(20.0, 10.0, 10.0, 5.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Center(
+                                child: Text (
+                                  "${tasks[index].text}",
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 15.0),
+                        child: Text("0 / 0"),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            BlocProvider.of<ProviderBloc>(context).add(DialogEvent(null, null));
+          },
+          label: Text('Task'),
+          icon: Icon(Icons.add),
+          backgroundColor: Theme.of(context).accentColor,
+        ),
+      );
+    });
   }
 }
