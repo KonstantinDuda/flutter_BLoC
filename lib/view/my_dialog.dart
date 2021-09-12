e9cadb15c6bbd44dfef8fbad8b8baed55ab50eef
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2021-09-12 17:30:51 +0300

    Убрал синюю полосу внизу диалога

--- lib/view/my_dialog.dart
+++ lib/view/my_dialog.dart
@@ -35,11 +35,11 @@ class MyDialog extends StatelessWidget {
       }
     }
     
-    return Scaffold(
-      body: // Column(
-        //children: <Widget>[
-          //Expanded ( child:
-          Container(
+    return  WillPopScope(
+      onWillPop: () async { BlocProvider.of<ProviderBloc>(context).add(RootEvent());
+      return false; },
+      child: Scaffold(
+      body: Container(
             //color: Theme.of(context).primaryColor,
             width: MediaQuery.of(context).size.width, // * 0.7,
             height: MediaQuery.of(context).size.height,
@@ -54,12 +54,14 @@ class MyDialog extends StatelessWidget {
               ),
               child:  
               TextField(
+                
                 decoration: InputDecoration(
                   contentPadding: EdgeInsets.fromLTRB(10, 10 ,10, 10),
+                  border: InputBorder.none,
                 ),
                 onChanged: (value) { newText = value; print('$newText'); return newText;},
                 controller: TextEditingController(text: newText),
-                cursorColor: Colors.black,
+                cursorColor: Theme.of(context).textTheme.headline5.color,
                 expands: true,
                 style: Theme.of(context).textTheme.headline5,
                 maxLines: null,
@@ -68,233 +70,9 @@ class MyDialog extends StatelessWidget {
               ),
             ),
           ),
-          //),
-          //VerticalDivider(width: 5.0,),
-          //Column(
-            //Divider(height: 5.0,),
             floatingActionButton: myFloatingActionButton(context),
-            /*Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              GestureDetector(
-                child: Container(
-                  //margin: EdgeInsets.fromLTRB(0, 2.5, 2.5, 2.5),
-                  height: MediaQuery.of(context).size.height * 0.2, //0.29,
-                  width: MediaQuery.of(context).size.width *0.35, //0.25,
-                  decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              color: Theme.of(context).primaryColor,),
-                  //color: Theme.of(context).primaryColor,
-                  child: Center(
-                    child: Text(
-                      'Cancel',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  ),
-                ),
-                onTap: () {
-                    if(chackTask != null) {
-                      BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
-                    } else {
-                      BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                    }
-                  /*} else {
-                    if(rootTask != null) {
-                      BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
-                    } else {
-                      BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                    }
-                  }*/
-                  
-                },
-              ),
-              VerticalDivider(width: 5.0,),
-              GestureDetector(
-                child: Container(
-                  //margin: EdgeInsets.fromLTRB(0, 2.5, 2.5, 2.5),
-                  height: MediaQuery.of(context).size.height * 0.2, // 0.29,
-                  width: MediaQuery.of(context).size.width * 0.35,// 0.25,
-                  decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              color: Theme.of(context).primaryColor),
-                  //color: Theme.of(context).primaryColor,
-                  child: Center(
-                    child: Text(
-                      'Save',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  ),
-                ),
-                onTap: () {
-                  print("Go to the Root Page");
-                  if(changeObj == true) {
-                      if(chackTask != null && rootTask != null) {
-                        print('if(chackTask != null && rootTask != null)');
-                        BlocProvider.of<ChackTaskBloc>(context).add(ChackTaskUpdateEvent(chackTask.id, 0, newText, false));
-                        BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
-                      } else if(rootTask != null && chackTask == null) {
-                        print('if(rootTask != null && chackTask == null)');
-                        BlocProvider.of<TaskBloc>(context).add(RootTaskUpdateEvent(rootTask.id, 0, newText, 0, 0));
-                        BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                      }
-                    } else {
-                      if(rootTask != null) {
-                        print('if(rootTask != null)');
-                        BlocProvider.of<ChackTaskBloc>(context).add(ChackTaskAddedEvent(newText, rootTask.id));
-                        BlocProvider.of<TaskBloc>(context).add(RootTaskUpdateEvent(rootTask.id, 0, rootTask.text, 0, 1));
-                        BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
-                      } else {
-                        print('else');
-                        BlocProvider.of<TaskBloc>(context).add(RootTaskAddedEvent(newText));
-                        BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                      }
-                    } 
-                  }
-                  /*if(rootTask == null) {
-                    print("TaskAddedEvent($newText) in Dialog");
-                    BlocProvider.of<TaskBloc>(context).add(RootTaskAddedEvent(newText));
-                    BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                  } else {
-                    BlocProvider.of<TaskBloc>(context).add(RootTaskUpdateEvent(rootTask.id, 0, newText));
-                    BlocProvider.of<ProviderBloc>(context).add(UpdateEvent(rootTask));
-                  }*/
-                //},
-              ),
-              //Divider(height: 5.0,),
-            ],
-          ),*/
-        //],
-      //),
+    )
     );
-    /*return Scaffold(
-      body: /*Row(*/ Column(
-        children: <Widget>[
-          Expanded ( child:
-          Container(
-            //color: Theme.of(context).primaryColor,
-            width: MediaQuery.of(context).size.width, // * 0.7,
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              color: Theme.of(context).primaryColor,),
-            child: Container(
-              margin: EdgeInsets.fromLTRB(5, 25, 5, 5), //EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                color: Theme.of(context).textTheme.headline6.color,
-              ),
-              child:  
-              TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.fromLTRB(10, 10 ,10, 10),
-                ),
-                onChanged: (value) => newText = value,
-                controller: TextEditingController(text: newText),
-                cursorColor: Colors.black,
-                expands: true,
-                style: Theme.of(context).textTheme.headline5,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                autofocus: true,
-              ),
-            ),
-          ),),
-          //VerticalDivider(width: 5.0,),
-          //Column(
-            //Divider(height: 5.0,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              GestureDetector(
-                child: Container(
-                  //margin: EdgeInsets.fromLTRB(0, 2.5, 2.5, 2.5),
-                  height: MediaQuery.of(context).size.height * 0.2, //0.29,
-                  width: MediaQuery.of(context).size.width *0.35, //0.25,
-                  decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              color: Theme.of(context).primaryColor,),
-                  //color: Theme.of(context).primaryColor,
-                  child: Center(
-                    child: Text(
-                      'Cancel',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  ),
-                ),
-                onTap: () {
-                    if(chackTask != null) {
-                      BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
-                    } else {
-                      BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                    }
-                  /*} else {
-                    if(rootTask != null) {
-                      BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
-                    } else {
-                      BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                    }
-                  }*/
-                  
-                },
-              ),
-              VerticalDivider(width: 5.0,),
-              GestureDetector(
-                child: Container(
-                  //margin: EdgeInsets.fromLTRB(0, 2.5, 2.5, 2.5),
-                  height: MediaQuery.of(context).size.height * 0.2, // 0.29,
-                  width: MediaQuery.of(context).size.width * 0.35,// 0.25,
-                  decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30.0)),
-              color: Theme.of(context).primaryColor),
-                  //color: Theme.of(context).primaryColor,
-                  child: Center(
-                    child: Text(
-                      'Save',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  ),
-                ),
-                onTap: () {
-                  print("Go to the Root Page");
-                  if(changeObj == true) {
-                      if(chackTask != null && rootTask != null) {
-                        print('if(chackTask != null && rootTask != null)');
-                        BlocProvider.of<ChackTaskBloc>(context).add(ChackTaskUpdateEvent(chackTask.id, 0, newText, false));
-                        BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
-                      } else if(rootTask != null && chackTask == null) {
-                        print('if(rootTask != null && chackTask == null)');
-                        BlocProvider.of<TaskBloc>(context).add(RootTaskUpdateEvent(rootTask.id, 0, newText, 0, 0));
-                        BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                      }
-                    } else {
-                      if(rootTask != null) {
-                        print('if(rootTask != null)');
-                        BlocProvider.of<ChackTaskBloc>(context).add(ChackTaskAddedEvent(newText, rootTask.id));
-                        BlocProvider.of<TaskBloc>(context).add(RootTaskUpdateEvent(rootTask.id, 0, rootTask.text, 0, 1));
-                        BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
-                      } else {
-                        print('else');
-                        BlocProvider.of<TaskBloc>(context).add(RootTaskAddedEvent(newText));
-                        BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                      }
-                    } 
-                  }
-                  /*if(rootTask == null) {
-                    print("TaskAddedEvent($newText) in Dialog");
-                    BlocProvider.of<TaskBloc>(context).add(RootTaskAddedEvent(newText));
-                    BlocProvider.of<ProviderBloc>(context).add(RootEvent());
-                  } else {
-                    BlocProvider.of<TaskBloc>(context).add(RootTaskUpdateEvent(rootTask.id, 0, newText));
-                    BlocProvider.of<ProviderBloc>(context).add(UpdateEvent(rootTask));
-                  }*/
-                //},
-              ),
-              //Divider(height: 5.0,),
-            ],
-          ),
-        ],
-      ),
-    ); */
   }
 
   myFloatingActionButton(BuildContext context) {
@@ -308,7 +86,7 @@ class MyDialog extends StatelessWidget {
               Container(
                 margin: EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 5.0),
                 child: FloatingActionButton.extended(
-                  label: Text('Cancel'),
+                  label: Text('Закрыть'),
                   onPressed: () {
                     if(chackTask != null) {
                       BlocProvider.of<ProviderBloc>(context).add(ChackEvent(rootTask));
@@ -322,7 +100,7 @@ class MyDialog extends StatelessWidget {
               Container(
                 margin: EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 5.0),
                 child: FloatingActionButton.extended(
-                  label: Text('Save'),
+                  label: Text('Сохранить'),
                   onPressed: () {
                     if(changeObj == true) {
                       if(chackTask != null && rootTask != null) {
